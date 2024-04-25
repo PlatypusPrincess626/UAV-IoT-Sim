2bd1040c023c0e4d3bd0437c5406e082f09bd76c
PlatypusPrincess626 <masoncoleconkel@outlook.com>
2024-04-24 21:07:01 -0500

    Editted Run

--- UAV_IoT_Sim/IoT_Device.py
+++ UAV_IoT_Sim/IoT_Device.py
@@ -193,11 +193,11 @@ class IoT_Device:
 
         sensMapping = [[0] * 3 for _ in range(5)]
         count = 0
-        for sens in range(self.sens_table.size):
-            if not (self.sens_table.iloc[sens - 1, 1]) and (count < 5):
+        for sens in range(len(self.sens_table.size)):
+            if not (self.sens_table.iloc[sens, 1]) and (count < 5):
                 sensMapping[count][0], sensMapping[count][1], sensMapping[count][2] = sens, \
-                    math.sqrt(pow((self.indX - self.sens_table.iloc[sens - 1, 0].indX), 2) + \
-                              pow((self.indY - self.sens_table.iloc[sens - 1, 0].indY), 2)), (-5 + count)
+                    math.sqrt(pow((self.indX - self.sens_table.iloc[sens, 0].indX), 2) + \
+                              pow((self.indY - self.sens_table.iloc[sens, 0].indY), 2)), (-5 + count)
                 state[sensMapping[count][2]][1], state[sensMapping[count][2]][2] = sensMapping[count][1], \
                     self.sens_table.iloc[sens - 1, 2]
             count += 1
