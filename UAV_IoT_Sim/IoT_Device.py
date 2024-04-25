cf8a1bb38e69b5492f5dc77111950fdf3644d6e1
PlatypusPrincess626 <masoncoleconkel@outlook.com>
2024-04-25 18:41:31 -0500

    Editted Run

--- UAV_IoT_Sim/IoT_Device.py
+++ UAV_IoT_Sim/IoT_Device.py
@@ -238,7 +238,7 @@ class IoT_Device:
 
         action = model.act(state)
 
-        if action <= len(state) - 5:
+        if action <= len(full_state):
             return True, True, full_state.iloc[action + 1, 0], _, state, _, action, _
         else:
             sensor = self.sens_table.iloc[sensMapping[action - len(state) + 1][0], 0]
