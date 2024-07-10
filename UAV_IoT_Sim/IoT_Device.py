c74394b8a10d6c080b7afa7c2c35fbfe09b6a6ce
PlatypusPrincess626 <masoncoleconkel@outlook.com>
2024-07-09 22:41:26 -0500

    New Input Structure

--- UAV_IoT_Sim/IoT_Device.py
+++ UAV_IoT_Sim/IoT_Device.py
@@ -173,7 +173,10 @@ class IoT_Device:
                 self.sens_table.iloc[sensor + channel, 1] = False
 
         self.stored_energy -= round(self._comms.get("LoRa_Current_A") * 30 * totalChannels * 1000)
-        self.max_AoI = self.sens_table.loc[:,'AoI'].max()
+        self.max_AoI = 0
+        for AoI in range(len(self.sens_table)):
+            if self.sens_table.loc[AoI, 'AoI'] > self.max_AoI:
+                self.max_AoI = self.sens_table.loc[AoI, 'AoI']
 
     def ch_upload(self, X: int, Y: int):
         if self.solar_powered:
