01ef5ad37545abdf6f40ea173b50c0d2e29f38b5
PlatypusPrincess626 <masoncoleconkel@outlook.com>
2024-08-30 01:17:03 -0500

    New File Model

--- run.py
+++ run.py
@@ -396,7 +396,7 @@ def prepopulate(agent, prepop_steps, env):
                 done = True
 
             if train_model:
-                print("Baa")
+                print("Raaa")
             # if True:
                 # agent.update(old_state, old_action, env.curr_reward, env.curr_state, buffer_done)
                 # DDQN
