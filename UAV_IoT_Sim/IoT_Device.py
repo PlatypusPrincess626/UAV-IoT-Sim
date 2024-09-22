bdc7298ab9d9efa68ad63374b1289421e1a731fd
PlatypusPrincess626 <masoncoleconkel@outlook.com>
2024-09-22 03:34:03 -0500

    New File Model

--- UAV_IoT_Sim/IoT_Device.py
+++ UAV_IoT_Sim/IoT_Device.py
@@ -155,13 +155,13 @@ class IoT_Device:
         power_upkeep = round(self.cpu_amps + self._comms.get("AmBC_Current_A"))
         if self.type == 2:
             power_upkeep += round(self._comms.get("Lora_Upkeep_A"))
-            print(round((power / self._comms.get("LoRa_Voltage_V")) * 1_000_000), self.stored_data)
         self.stored_energy -= power_upkeep
 
     # Uploading data from a sensor
     def ws_upload_data(self, X, Y):
         if math.sqrt(pow((self.indX - X), 2) + pow((self.indY - Y), 2)) <= self._comms.get("AmBC_Max_Distance_m"):
             temp = min(self._comms.get("AmBC_Bit_Rate_bit/s") * 26, self.stored_data)
+            print(temp)
             self.stored_data -= temp
             return temp
         else:
