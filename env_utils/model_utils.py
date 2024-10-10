b15712f956f88ab0c40e311409803064d157b056
PlatypusPrincess626 <masoncoleconkel@outlook.com>
2024-10-10 18:57:06 -0500

    New File Model

--- env_utils/model_utils.py
+++ env_utils/model_utils.py
@@ -444,19 +444,19 @@ class get_ddqn_regression_agent():
         self.model_target.set_weights(self.model.get_weights())
 
     def act(self, state):
-        r_state = [state[0]/self.state1_max, state[1]/self.state2_max]
+        r_state = [state[0]/self.state1_max, state[1]/6_800_000, state[2]/self.state2_max]
         action_vals = self.model.predict(np.reshape(np.array(r_state), (-1, self.nA)))  # Exploit: Use the NN to predict the correct action from this state
         return action_vals[0]
 
     def test_action(self, state):  # Exploit
-        r_state = [state[0]/self.state1_max, state[1]/self.state2_max]
+        r_state = [state[0]/self.state1_max, state[1]/6_800_000, state[2]/self.state2_max]
         action_vals = self.model.predict(np.reshape(np.array(r_state), (-1, self.nA))) # Exploit: Use the NN to predict the correct action from this state
         return action_vals[0]
 
     def update_mem(self, state, action, reward, nstate, done):
         # Store the experience in memory
-        r_state = [state[0]/self.state1_max, state[1]/self.state2_max]
-        r_nstate = [nstate[0]/self.state1_max, nstate[1]/self.state2_max]
+        r_state = [state[0]/self.state1_max, state[1]/6_800_000, state[2]/self.state2_max]
+        r_nstate = [nstate[0]/self.state1_max, nstate[1]/6_800_000, nstate[2]/self.state2_max]
         self.memory.append((r_state, action, reward, r_nstate, done))
 
     def train(self, batch_size):
