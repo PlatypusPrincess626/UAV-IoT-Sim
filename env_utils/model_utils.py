42a58e56f41c76228b85d70b21416c64b8b626b6
PlatypusPrincess626 <masoncoleconkel@outlook.com>
2024-10-10 19:03:25 -0500

    New File Model

--- env_utils/model_utils.py
+++ env_utils/model_utils.py
@@ -445,13 +445,13 @@ class get_ddqn_regression_agent():
 
     def act(self, state):
         r_state = [state[0]/self.state1_max, state[1]/6_800_000, state[2]/self.state2_max]
-        print(np.reshape(np.array(r_state), (-1, self.nA)).shape)
-        action_vals = self.model.predict(np.reshape(np.array(r_state), (-1, self.nA)))  # Exploit: Use the NN to predict the correct action from this state
+        print(np.reshape(np.array(r_state), (self.nA)).shape)
+        action_vals = self.model.predict(np.reshape(np.array(r_state), (self.nA)))  # Exploit: Use the NN to predict the correct action from this state
         return action_vals[0]
 
     def test_action(self, state):  # Exploit
         r_state = [state[0]/self.state1_max, state[1]/6_800_000, state[2]/self.state2_max]
-        action_vals = self.model.predict(np.reshape(np.array(r_state), (-1, self.nA))) # Exploit: Use the NN to predict the correct action from this state
+        action_vals = self.model.predict(np.reshape(np.array(r_state), (self.nA))) # Exploit: Use the NN to predict the correct action from this state
         return action_vals[0]
 
     def update_mem(self, state, action, reward, nstate, done):
