baedf4517ab5e64017e8c2d5a405945b826eee9d
PlatypusPrincess626 <masoncoleconkel@outlook.com>
2024-10-15 22:00:17 -0500

    New File Model

--- UAV_IoT_Sim/IoT_Device.py
+++ UAV_IoT_Sim/IoT_Device.py
@@ -392,5 +392,6 @@ class IoT_Device:
 
         p_state = [d_to_targ, state[0][2], AoI_peak + math.floor(30 * self.action_p[0])]
         action_p = model_p.act(p_state)
+        print(action_p)
 
         return model_help, True, target, state, out_state, action, self.action_p, p_state
