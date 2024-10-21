85434f49666ba9e6716856fd301604c8735e3733
PlatypusPrincess626 <masoncoleconkel@outlook.com>
2024-10-21 16:51:18 -0500

    New File Model

--- UAV_IoT_Sim/UAV_IoT_Sim.py
+++ UAV_IoT_Sim/UAV_IoT_Sim.py
@@ -104,7 +104,7 @@ class make_env:
     def step(self, model, model_p):
         train_model = False
         old_paction = 0.0
-        excess_energy = 0
+        excess_energy = 1.0
         old_action = 0
         comms, move, harvest = 0, 0, 0
         old_pstate = [0, 0, 0]
