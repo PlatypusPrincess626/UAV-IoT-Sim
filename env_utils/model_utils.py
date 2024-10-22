1cfcb443f764c8ea30744aaf5eede7e74ad51b74
PlatypusPrincess626 <masoncoleconkel@outlook.com>
2024-10-22 18:21:01 -0500

    New File Model

--- env_utils/model_utils.py
+++ env_utils/model_utils.py
@@ -454,7 +454,7 @@ class get_ddqn_regression_agent():
             return result
 
         action_vals = self.model.predict(np.reshape(np.array(r_state), (-1, self.nS)))  # Exploit: Use the NN to predict the correct action from this state
-        print(action_vals.shape)
+        print(action_vals[0])
         return action_vals[0]
 
     def test_action(self, state):  # Exploit
