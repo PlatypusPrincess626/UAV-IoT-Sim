c3260cdd9c03fc6c7ed1f365c78f75defd57c123
PlatypusPrincess626 <masoncoleconkel@outlook.com>
2024-11-14 18:46:01 -0600

    New File Model

--- UAV_IoT_Sim/UAV_IoT_Sim.py
+++ UAV_IoT_Sim/UAV_IoT_Sim.py
@@ -36,6 +36,12 @@ class make_env:
 
         self.full_reward = 0
         self.full_reward2 = 0
+        self.accum_reward = 0
+        self.accum_reward_p = 0
+        self.accum_steps = 0
+        self.accum_steps_p = 0
+        self.track_reward = False
+        self.track_reward_p = False
 
         self.curr_state = [[0, 0, 0] for _ in range(self.num_ch + 1)]
         self.archived_state = [[0, 0, 0] for _ in range(self.num_ch + 1)]
@@ -82,6 +88,12 @@ class make_env:
 
             self.full_reward = 0
             self.full_reward2 = 0
+            self.accum_reward = 0
+            self.accum_reward_p = 0
+            self.accum_steps = 0
+            self.accum_steps_p = 0
+            self.track_reward = False
+            self.track_reward_p = False
 
             self.curr_state = [[0, 0, 0] for _ in range(self.num_ch + 1)]
             self.archived_state = [[0, 0, 0] for _ in range(self.num_ch + 1)]
@@ -116,12 +128,6 @@ class make_env:
         comms, move, harvest = 0, 0, 0
         old_pstate = [0, 0, 0]
         self.reward2 = 0
-        accum_reward = 0
-        accum_reward_p = 0
-        accum_steps = 0
-        accum_steps_p = 0
-        track_reward = False
-        track_reward_p = False
 
         old_state = [[0, 0, 0] for _ in range(self.num_ch + 1)]
 
@@ -152,7 +158,7 @@ class make_env:
                     # train_model = True
                     if train_model or train_model2:
                         train_model = True
-                        self.full_reward = accum_reward / max(accum_steps, 1)
+                        self.full_reward = self.accum_reward / max(self.accum_steps, 1)
 
                     self.curr_state = uav.state
                     self.last_action = action
@@ -168,23 +174,23 @@ class make_env:
                         if self.curr_step < 15:
                             train_p = False
                         else:
-                            self.full_reward2 = accum_reward_p / accum_steps_p
+                            self.full_reward2 = self.accum_reward_p / max(self.accum_steps_p, 1)
                         self.archived_pstate = p_state
                         self.archived_paction = action_p
-                        track_reward_p = True
+                        self.track_reward_p = True
 
                     if used_model:
                         self.archived_state = state
                         self.archived_action = action
-                        track_reward = True
+                        self.track_reward = True
 
                 self.reward(excess_energy)
-                if track_reward:
-                    accum_steps += 1
-                    accum_reward += self.curr_reward
-                if track_reward_p:
-                    accum_steps_p += 1
-                    accum_reward_p += self.reward2
+                if self.track_reward:
+                    self.accum_steps += 1
+                    self.accum_reward += self.curr_reward
+                if self.track_reward_p:
+                    self.accum_steps_p += 1
+                    self.accum_reward_p += self.reward2
                 self.curr_step += 1
             else:
                 self.truncated = True
