a2fe1cb58a486851279acdb9d24a0c96dcd5738a
PlatypusPrincess626 <masoncoleconkel@outlook.com>
2024-11-15 01:22:08 -0600

    New File Model

--- run.py
+++ run.py
@@ -198,7 +198,7 @@ def evaluate(
                 CHCoords.append([eval_env.chX[cluster], eval_env.chY[cluster]])
 
         if log_metrics and i == eval_episodes - 1:
-            filename = ("sens_pts_" + curr_date_time.strftime("%d") + "_" +
+            filename = ("sens_pts2_" + curr_date_time.strftime("%d") + "_" +
                         curr_date_time.strftime("%m") + ".csv")
             open(filename, 'x')
             with open(filename, 'w') as csvfile:
@@ -206,7 +206,7 @@ def evaluate(
                 csvwriter.writerows(SensCoords)
 
         if log_metrics and i == eval_episodes - 1:
-            filename = ("cluster_pts_" + curr_date_time.strftime("%d") + "_" +
+            filename = ("cluster_pts2_" + curr_date_time.strftime("%d") + "_" +
                         curr_date_time.strftime("%m") + ".csv")
             open(filename, 'x')
             with open(filename, 'w') as csvfile:
@@ -215,7 +215,7 @@ def evaluate(
 
         if log_metrics and i == eval_episodes - 1:
             print(eval_env.ch_sensors)
-            filename = ("age_metrics_" + curr_date_time.strftime("%d") + "_" +
+            filename = ("age_metrics2_" + curr_date_time.strftime("%d") + "_" +
                         curr_date_time.strftime("%m") + ".csv")
             open(filename, 'x')
             with open(filename, 'w') as csvfile:
@@ -223,7 +223,7 @@ def evaluate(
                 csvwriter.writerows(CH_Age)
 
         if log_metrics and i == eval_episodes - 1:
-            filename = ("data_metrics_" + curr_date_time.strftime("%d") + "_" +
+            filename = ("data_metrics2_" + curr_date_time.strftime("%d") + "_" +
                         curr_date_time.strftime("%m") + ".csv")
             open(filename, 'x')
             with open(filename, 'w') as csvfile:
@@ -231,7 +231,7 @@ def evaluate(
                 csvwriter.writerows(CH_Data)
 
         if log_metrics and i == eval_episodes - 1:
-            filename = ("uav_metrics_" + curr_date_time.strftime("%d") + "_" +
+            filename = ("uav_metrics2_" + curr_date_time.strftime("%d") + "_" +
                         curr_date_time.strftime("%m") + ".csv")
             open(filename, 'x')
             with open(filename, 'w') as csvfile:
