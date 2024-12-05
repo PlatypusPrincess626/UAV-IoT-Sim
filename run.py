896c63f1dcebbb4a70bb7fef7fcde9ae905c0cc5
PlatypusPrincess626 <masoncoleconkel@outlook.com>
2024-12-05 03:13:20 -0600

    New File Model

--- run.py
+++ run.py
@@ -99,7 +99,6 @@ def get_args():
 
 def evaluate(
         agent,
-        agent_p,
         eval_env,
         eval_episodes,
         log_metrics=False,
@@ -132,7 +131,6 @@ def evaluate(
 
     # QL
     agent.decay_epsilon(1)
-    agent_p.decay_epsilon(1)
 
     for i in range(eval_episodes):
         eval_env.reset()
@@ -147,8 +145,7 @@ def evaluate(
         CH_Metrics = [[0, 0] for _ in range(eval_env.num_ch)]
 
         while not done:
-            train_model, train_p, old_state, old_action, action_p, old_pstate, comms, move, harvest = eval_env.step(
-                agent, agent_p)
+            train_model, old_state, old_action, comms, move, harvest = eval_env.step(agent)
             buffer_done = eval_env.terminated
             info = eval_env.curr_info
             crashed = eval_env.terminated
@@ -156,7 +153,6 @@ def evaluate(
             if buffer_done or eval_env.truncated:
                 done = True
 
-            print(eval_env.accum_reward_p / max(eval_env.accum_steps_p, 1))
             avgAoI += info.get("Avg_Age", 0.0)
             peakAoI += info.get("Peak_Age", 0.0)
             dataDist += info.get("Data_Distribution", 0.0)
@@ -170,10 +166,7 @@ def evaluate(
                 CH_Metrics[ch][0] = eval_env.curr_state[ch + 1][1]
                 CH_Metrics[ch][1] = eval_env.curr_state[ch + 1][2]
 
-            if train_p or done:
-                agent_p.update_mem(old_pstate, int(action_p), eval_env.full_reward2, eval_env.curr_pstate, buffer_done)
-
-            if (train_model or eval_env.truncated) and not buffer_done:
+            if train_model or done:
                 # if True:
                 #     agent.update(old_state, old_action, eval_env.curr_reward, eval_env.curr_state, buffer_done)
                 # DDQN
@@ -237,12 +230,6 @@ def evaluate(
                 csvwriter = csv.writer(csvfile, delimiter='|')
                 csvwriter.writerows(UAV_Metrics)
 
-        # DDQN
-        if len(agent.memory) > 64:
-            agent.train(64)
-        if len(agent_p.memory) > 64:
-            agent_p.train(64)
-
         accum_avgAoI += avgAoI / (eval_env.curr_step + count)
         accum_peakAoI += peakAoI / (eval_env.curr_step + count)
         accum_dataDist += dataDist / (eval_env.curr_step + count)
@@ -269,16 +256,12 @@ def evaluate(
 
 def train(
         agent,
-        agent_p,
         env: object,
         env_str: str,
         total_steps: int,
         eval_frequency: int,
         eval_episodes: int,
         policy_path: str,
-        mean_success_rate: RunningAverage,
-        mean_episode_length: RunningAverage,
-        mean_reward: RunningAverage,
         logger=None,
 ):
     start_time = time()
@@ -286,105 +269,41 @@ def train(
     env.reset()
     sr, ret, length = 0.0, 0.0, 0.0
     for timestep in range(total_steps):
-        done = step(agent, agent_p, env)
+        done = step(agent, env)
         if done:
-            if len(agent.memory) > 64:
-                agent.train(64)
-            if len(agent_p.memory) > 64:
-                agent_p.train(64)
+            if len(agent.memory) > 128:
+                agent.train(128)
         # QL
         agent.decay_epsilon(timestep / total_steps)
-        agent_p.decay_epsilon(1)
 
         if timestep % (2 * eval_frequency) == 0:
             # DDQN
             agent.update_target_from_model()
-            agent_p.update_target_from_model()
             env.reset()
 
         if timestep % eval_frequency == 0:
-            hours = (time() - start_time) / 3600
-            log_vals = {
-                "losses/TD_Error": agent.td_errors.mean(),
-                "losses/Grad_Norm": agent.grad_norms.mean(),
-                "losses/Max_Q_Value": agent.qvalue_max.mean(),
-                "losses/Mean_Q_Value": agent.qvalue_mean.mean(),
-                "losses/Min_Q_Value": agent.qvalue_min.mean(),
-                "losses/Max_Target_Value": agent.target_max.mean(),
-                "losses/Mean_Target_Value": agent.target_mean.mean(),
-                "losses/Min_Target_Value": agent.target_min.mean(),
-                "losses/hours": hours,
-            }
             sr, ret, length, avgAoI, peakAoI, dataDist, dataColl, CH_Metrics, \
-                comms, move, harvest = evaluate(agent, agent_p, env, eval_episodes)
-
-            log_vals.update(
-                {
-                    f"{env_str}/SuccessRate": sr,
-                    f"{env_str}/Return": ret,
-                    f"{env_str}/EpisodeLength": length,
-                    f"{env_str}/AverageAoI": avgAoI,
-                    f"{env_str}/PeakAoI": peakAoI,
-                    f"{env_str}/Distribution": dataDist,
-                    f"{env_str}/TotalCollected": dataColl,
-                }
-            )
-
-            # logger.log(
-            #     log_vals,
-            #     step=timestep,
-            # )
+                comms, move, harvest = evaluate(agent, env, eval_episodes)
 
         print(
             f"Training Steps: {timestep}, Env: {env_str}, Sucess Rate: {sr:.2f}, Return: {ret:.2f}, Episode Length: {length:.2f}"
         )
 
-    hours = (time() - start_time) / 3600
-    log_vals = {
-        "losses/TD_Error": agent.td_errors.mean(),
-        "losses/Grad_Norm": agent.grad_norms.mean(),
-        "losses/Max_Q_Value": agent.qvalue_max.mean(),
-        "losses/Mean_Q_Value": agent.qvalue_mean.mean(),
-        "losses/Min_Q_Value": agent.qvalue_min.mean(),
-        "losses/Max_Target_Value": agent.target_max.mean(),
-        "losses/Mean_Target_Value": agent.target_mean.mean(),
-        "losses/Min_Target_Value": agent.target_min.mean(),
-        "losses/hours": hours,
-    }
     sr, ret, length, avgAoI, peakAoI, dataDist, dataColl, CH_Metrics, \
-        comms, move, harvest = evaluate(agent, agent_p, env, eval_episodes, True, env_str, start_time)
-
-    log_vals.update(
-        {
-            f"{env_str}/SuccessRate": sr,
-            f"{env_str}/Return": ret,
-            f"{env_str}/EpisodeLength": length,
-            f"{env_str}/AverageAoI": avgAoI,
-            f"{env_str}/PeakAoI": peakAoI,
-            f"{env_str}/Distribution": dataDist,
-            f"{env_str}/TotalCollected": dataColl,
-        }
-    )
-
-    # logger.log(
-    #     log_vals,
-    #     step=total_steps,
-    # )
+        comms, move, harvest = evaluate(agent, env, eval_episodes, True, env_str, start_time)
 
 
-def step(agent, agent_p, env):
-    train_model, train_p, old_state, old_action, action_p, old_pstate, comms, move, harvest = env.step(agent, agent_p)
+
+def step(agent, env):
+    train_model, old_state, old_action, comms, move, harvest = env.step(agent)
     buffer_done = env.terminated
     done = False
 
     if buffer_done or env.truncated:
         done = True
 
-    if train_p or done:
-        agent_p.update_mem(old_pstate, int(action_p), env.full_reward2, env.curr_pstate, buffer_done)
-    if (train_model or env.truncated) and not buffer_done:
+    if train_model or done:
         # if True:
-        print(f"Training")
         # QL
         # agent.update(old_state, old_action, env.curr_reward, env.curr_state, buffer_done)
         # DDQN
@@ -394,7 +313,7 @@ def step(agent, agent_p, env):
 
 def prepopulate(agent, prepop_steps, env):
     # QL
-    # agent.decay_epsilon(0)
+    agent.decay_epsilon(0)
 
     timestep = 0
     while timestep < prepop_steps:
@@ -409,7 +328,7 @@ def prepopulate(agent, prepop_steps, env):
             if buffer_done or env.truncated:
                 done = True
 
-            if (train_model or env.truncated) and not buffer_done:
+            if train_model or done:
                 agent.update_mem(old_state, old_action, env.full_reward, env.curr_state, buffer_done)
             timestep += 1
 
@@ -459,11 +378,7 @@ def run_experiment(args):
         args.steps,
         args.eval_frequency,
         args.eval_episodes,
-        policy_path,
-        mean_success_rate,
-        mean_episode_length,
-        mean_reward,
-        # logger
+        policy_path
     )
 
 
