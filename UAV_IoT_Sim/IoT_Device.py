e36b11afa174c690e30620f47df9ba59cc8fb29f
PlatypusPrincess626 <masoncoleconkel@outlook.com>
2025-01-17 17:50:35 -0600

    New model

--- UAV_IoT_Sim/IoT_Device.py
+++ UAV_IoT_Sim/IoT_Device.py
@@ -394,7 +394,8 @@ class IoT_Device:
             self.last_target = self.headSerial
             target = self.tour[0]
             print("Here")
-            print(target.type)
+            print(len(self.tour))
+            print("Gone")
             self.target_time = step
             action = self.headSerial
             model_help = False
