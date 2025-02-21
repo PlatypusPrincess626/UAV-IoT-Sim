c622b4d5ff325460b9a679ee3b73eda3435be453
PlatypusPrincess626 <masoncoleconkel@outlook.com>
2025-02-21 00:07:20 -0600

    New model

--- run.py
+++ run.py
@@ -208,7 +208,7 @@ def evaluate(
             for cluster in range(len(eval_env.chX)):
                 CHCoords.append([eval_env.chX[cluster], eval_env.chY[cluster]])
 
-        csv_str = ("_Dual3K.csv")
+        csv_str = ("_Dual_100K_3K.csv")
 
         if log_metrics and i == eval_episodes - 1:
             filename = ("sens_pts_" + curr_date_time.strftime("%d") + "_" +
@@ -539,7 +539,7 @@ def run_experiment(args):
         f"model={args.model}"
     )
 
-    prepopulate(agent, agent_p, 0, env, args.eval_frequency)
+    prepopulate(agent, agent_p, 100_000, env, args.eval_frequency)
     mean_success_rate = RunningAverage(10)
     mean_reward = RunningAverage(10)
     mean_episode_length = RunningAverage(10)
