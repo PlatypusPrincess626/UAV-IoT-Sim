a167d043ea205b5c554fc3a117b5535ad47a27b7
PlatypusPrincess626 <masoncoleconkel@outlook.com>
2025-03-17 16:43:24 -0500

    New model

--- env_utils/model_utils.py
+++ env_utils/model_utils.py
@@ -280,7 +280,7 @@ class get_ddqn_agent():
     def build_model(self):
         model = tf.keras.Sequential()  # linear stack of layers https://keras.io/models/sequential/
         model.add(tf.keras.layers.Input(shape=(self.nS, )))
-        model.add(tf.keras.layers.SimpleRNN(256, activation='relu'), dropout=0.2)  # [Input] -> Layer 1
+        model.add(tf.keras.layers.SimpleRNN(256, activation='relu', dropout=0.2))  # [Input] -> Layer 1
         model.add(tf.keras.layers.Dense(126, activation='relu'))  # Layer 1 -> 2
         model.add(tf.keras.layers.Dropout(0.2))
         model.add(tf.keras.layers.Dense(self.nA, activation='softmax'))  # Layer 2 -> [output]
