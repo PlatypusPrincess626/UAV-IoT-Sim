3aa8188f03efa6079de77e7efa3722298ee6db48
PlatypusPrincess626 <masoncoleconkel@outlook.com>
2025-04-25 13:38:39 -0500

    New model

--- run.py
+++ run.py
@@ -211,7 +211,7 @@ def evaluate(
             for cluster in range(len(eval_env.chX)):
                 CHCoords.append([eval_env.chX[cluster], eval_env.chY[cluster]])
 
-        csv_str = ("_Dual_NForced_500K_3K.csv")
+        csv_str = ("_Dual_NForced_500K_3K_2.csv")
 
         if log_metrics and i == eval_episodes - 1:
             filename = ("sens_pts_" + curr_date_time.strftime("%d") + "_" +
